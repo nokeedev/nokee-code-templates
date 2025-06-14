92e1ea59ef9cf41cbf90e63d54fec0f3b367060b
Daniel Lacasse <daniel@nokee.dev>
2025-06-14 22:26:06 +0200

    Include templates-cpp-list
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ src/main/java/dev/nokee/templates/CppListLib.java
@@ -0,0 +1,8 @@
+package dev.nokee.templates;
+
+import dev.gradleplugins.fixtures.sources.NativeLibraryElement;
+import dev.gradleplugins.fixtures.sources.annotations.SourceProject;
+
+@SourceProject("templates-cpp-list/cpp-list")
+public final class CppListLib extends NativeLibraryElement.FromResource {
+}
---
+++ src/main/java/dev/nokee/templates/CppUtilitiesLib.java
@@ -0,0 +1,7 @@
+package dev.nokee.templates;
+
+import dev.gradleplugins.fixtures.sources.NativeLibraryElement;
+import dev.gradleplugins.fixtures.sources.annotations.SourceProject;
+
+@SourceProject("templates-cpp-list/cpp-utilities")
+public final class CppUtilitiesLib extends NativeLibraryElement.FromResource {}
